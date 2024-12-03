a44b87664a655417f71d742360dbc98a51d50ad5
Scott Aron Bloom <scott@towel42.com>
2024-12-02 17:15:19 -0800

    Fixes for dumpcpp

--- main/include.cmake
+++ main/include.cmake
@@ -11,8 +11,11 @@ if( NOT DUMPCPP_EXECUTABLE )
     if( NOT DUMPCPP_EXECUTABLE )
         MESSAGE( WARNING "Could not find build area dumpcpp.  Re-run cmake after initial build" )
     else()
+        file( REAL_PATH ${DUMPCPP_EXECUTABLE} DUMPCPP_EXECUTABLE EXPAND_TILDE)
+        file( TO_CMAKE_PATH ${DUMPCPP_EXECUTABLE} DUMPCPP_EXECUTABLE)
+        
         mark_as_advanced(DUMPCPP_EXECUTABLE)
-        MESSAGE( STATUS "Found dumpcpp: ${DUMPCPP_EXECUTABLE}" )
+        MESSAGE( STATUS "Found dumpcpp: '${DUMPCPP_EXECUTABLE}'" )
     endif()
 endif()
 
@@ -21,7 +24,7 @@ set( MSOUTL_OLB "C:/Program Files (x86)/Microsoft Office/Root/Office16/MSOUTL.OL
 ADD_CUSTOM_COMMAND( 
     OUTPUT 
         ${CMAKE_CURRENT_BINARY_DIR}/MSOUTL.cpp ${CMAKE_CURRENT_BINARY_DIR}/MSOUTL.h
-    COMMAND "${DUMPCPP_EXECUTABLE}" "${MSOUTL_OLB}" -o MSOUTL
+    COMMAND ${CMAKE_COMMAND} -E env "PATH=C:/Qt/Qt5.12.12/5.12.12/msvc2017_64/bin;$ENV{PATH}" "${DUMPCPP_EXECUTABLE}" "${MSOUTL_OLB}" -o MSOUTL
     COMMENT "[DUMPCPP] Generating MSOUTL.cpp and MSOUT.h from ${MSOUTL_OLB}"
     VERBATIM
     DEPENDS
