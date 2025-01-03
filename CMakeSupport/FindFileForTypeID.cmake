da85f3ab9fb198fae436051b42d1257a80284a27
Scott Aron Bloom <scott@towel42.com>
2025-01-03 12:36:53 -0800

    Reorganize file id

--- CMakeSupport/FindFileForTypeID.cmake
+++ CMakeSupport/FindFileForTypeID.cmake
@@ -9,8 +9,33 @@
 #*******************************************************************************
 #*******************************************************************************
 
+if( NOT DUMPCPP_EXECUTABLE )
+    find_program(DUMPCPP_EXECUTABLE NAMES sab_dumpcpp
+      PATHS ${CMAKE_INSTALL_PREFIX} ${CMAKE_BINARY_DIR}/dumpcpp/RelWithDebInfo ${CMAKE_BINARY_DIR}/dumpcpp/Release ${CMAKE_BINARY_DIR}/dumpcpp/Debug
+        DOC "path to the dumpcpp executable (from build area)" 
+        NO_DEFAULT_PATH
+#        NO_CACHE
+        )
+    if( NOT DUMPCPP_EXECUTABLE )
+        MESSAGE( WARNING "Could not find build area dumpcpp.  Re-run cmake after initial build" )
+    else()
+        file( REAL_PATH ${DUMPCPP_EXECUTABLE} DUMPCPP_EXECUTABLE EXPAND_TILDE)
+        file( TO_CMAKE_PATH ${DUMPCPP_EXECUTABLE} DUMPCPP_EXECUTABLE)
+        mark_as_advanced(DUMPCPP_EXECUTABLE)
+    endif()
+
+    if( DUMPCPP_EXECUTABLE )
+        execute_process( COMMAND ${DUMPCPP_EXECUTABLE} --version OUTPUT_VARIABLE DUMPCPP_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+        mark_as_advanced(DUMPCPP_VERSION)
+
+        MESSAGE( STATUS "Using dumpcpp: '${DUMPCPP_EXECUTABLE}' - ${DUMPCPP_VERSION}" )
+    endif()
+
+
+endif()
+
 MACRO(FileForTypeID typeID prefix )
-    set( ${prefix}_PATH FALSE)
+    set( ${prefix}_OLBPATH FALSE)
     
     string(CONCAT regPathBase "HKEY_LOCAL_MACHINE\\Software\\Classes\\TypeLib\\"  ${typeID} )
     #message( STATUS "regPathBase=${regPathBase}" )
@@ -28,11 +53,44 @@ MACRO(FileForTypeID typeID prefix )
             #MESSAGE( STATUS "path=${path}" )
             if ( EXISTS ${path} )
                 #MESSAGE( STATUS "prefix=${prefix}" )
-                set( ${prefix}_PATH ${path} )
-                #message( STATUS "${prefix}_PATH = ${${prefix}_PATH}" )
+                set( ${prefix}_OLBPATH ${path} )
+                #message( STATUS "${prefix}_OLBPATH = ${${prefix}_OLBPATH}" )
                 break()
             endif()
         endforeach()
     endforeach()
 ENDMACRO()
+
+
+MACRO( GenerateCPPFromFileID fileID prefix )
+    if( NOT DUMPCPP_EXECUTABLE )
+        MESSAGE( FATAL_ERROR "Could not find sab_dumpcpp" )
+    endif()
+
+    FileForTypeID( ${fileID} ${prefix} )
+
+    #message( STATUS "${prefix}_OLBPATH=${${prefix}_OLBPATH}" )
+    if ( NOT EXISTS ${${prefix}_OLBPATH} )
+        message( FATAL_ERROR "Could not find OLB file '${${prefix}_OLBPATH}' for file id ${fileID}" )
+    endif()
+
+    set( ${prefix}_CPP ${CMAKE_CURRENT_BINARY_DIR}/${prefix}.cpp )
+    set( ${prefix}_H ${CMAKE_CURRENT_BINARY_DIR}/${prefix}.h )
+
+    #message( STATUS "${prefix}_CPP=${${prefix}_CPP}" )
+    #message( STATUS "${prefix}_H=${${prefix}_H}" )
+    #message( STATUS "DUMPCPP_EXECUTABLE=${DUMPCPP_EXECUTABLE} - ${DUMPCPP_VERSION}" )
+
+    ADD_CUSTOM_COMMAND( 
+        OUTPUT 
+            ${${prefix}_CPP} ${${prefix}_H}
+        COMMAND ${CMAKE_COMMAND} -E env "PATH=${QT_MSVCDIR}/bin;$ENV{PATH}" "${DUMPCPP_EXECUTABLE}" "${fileID}" -o ${prefix}
+        COMMENT "[DUMPCPP] Generating ${prefix}.cpp and ${prefix}.h from '${${prefix}_OLBPATH}' using '${DUMPCPP_EXECUTABLE}'"
+        VERBATIM
+        DEPENDS
+           ${${prefix}_OLBPATH}
+           ${DUMPCPP_EXECUTABLE}
+    )
+endmacro()
+   
 
--- OutlookAPI/include.cmake
+++ OutlookAPI/include.cmake
@@ -1,43 +1,14 @@
 set(_PROJECT_NAME OutlookAPI)
 set(FOLDER_NAME libs)
 
-if( NOT DUMPCPP_EXECUTABLE )
-    find_program(DUMPCPP_EXECUTABLE NAMES sab_dumpcpp
-      PATHS ${CMAKE_INSTALL_PREFIX} ${CMAKE_BINARY_DIR}/dumpcpp/RelWithDebInfo ${CMAKE_BINARY_DIR}/dumpcpp/Release ${CMAKE_BINARY_DIR}/dumpcpp/Debug
-        DOC "path to the dumpcpp executable (from build area)" 
-        NO_DEFAULT_PATH
-#        NO_CACHE
-        )
-    if( NOT DUMPCPP_EXECUTABLE )
-        MESSAGE( WARNING "Could not find build area dumpcpp.  Re-run cmake after initial build" )
-    else()
-        file( REAL_PATH ${DUMPCPP_EXECUTABLE} DUMPCPP_EXECUTABLE EXPAND_TILDE)
-        file( TO_CMAKE_PATH ${DUMPCPP_EXECUTABLE} DUMPCPP_EXECUTABLE)
-        mark_as_advanced(DUMPCPP_EXECUTABLE)
-    endif()
-endif()
-
-
-if( DUMPCPP_EXECUTABLE )
-    MESSAGE( STATUS "Using dumpcpp: '${DUMPCPP_EXECUTABLE}'" )
-endif()
-
 find_package( FileForTypeID  )
-FileForTypeID( "{00062FFF-0000-0000-C000-000000000046}" MSOUTL_OLB )
-#message( STATUS "MSOUTL_OLB_PATH=${MSOUTL_OLB_PATH}" )
-if ( NOT EXISTS ${MSOUTL_OLB_PATH} )
-    message( FATAL_ERROR "Could not find OLB file for MS Outlook" )
-endif()
 
-ADD_CUSTOM_COMMAND( 
-    OUTPUT 
-        ${CMAKE_CURRENT_BINARY_DIR}/MSOUTL.cpp ${CMAKE_CURRENT_BINARY_DIR}/MSOUTL.h
-    COMMAND ${CMAKE_COMMAND} -E env "PATH=${QT_MSVCDIR}/bin;$ENV{PATH}" "${DUMPCPP_EXECUTABLE}" "${MSOUTL_OLB_PATH}" -o MSOUTL
-    COMMENT "[DUMPCPP] Generating MSOUTL.cpp and MSOUT.h from '${MSOUTL_OLB_PATH}' using '${DUMPCPP_EXECUTABLE}'"
-    VERBATIM
-    DEPENDS
-       ${MSOUTL_OLB}
-)
+GenerateCPPFromFileID( "{00062FFF-0000-0000-C000-000000000046}" MSOUTL )
+
+#message( "DUMPCPP=${DUMPCPP_EXECUTABLE}" )
+#message( "MSOUTL_OLBPATH=${MSOUTL_OLBPATH}" )
+#message( "MSOUTL_CPP=${MSOUTL_CPP}" )
+#message( "MSOUTL_H=${MSOUTL_H}" )
 
 set(qtproject_UIS
 )
