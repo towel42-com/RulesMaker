9cb438f1bc19d553d806df2aad9ff5bee7247736
Scott Aron Bloom <scott@towel42.com>
2025-03-05 23:54:44 +0000

    Clean up dumpcpp outputs

--- OutlookLib/include.cmake
+++ OutlookLib/include.cmake
@@ -1,28 +1,35 @@
 set(_PROJECT_NAME OutlookLib)
 set(FOLDER_NAME libs)
 
+#message( "DUMPCPP=${DUMPCPP_EXECUTABLE}" )
 find_package( FileForTypeID  )
+
 
 GenerateCPPFromFileID( "{00062FFF-0000-0000-C000-000000000046}" MSOUTL ol )
 
-#message( "DUMPCPP=${DUMPCPP_EXECUTABLE}" )
-#message( "MSOUTL_OLBPATH=${MSOUTL_OLBPATH}" )
+#message( "MSOUTL_TYPEID_FILEPATH=${MSOUTL_TYPEID_FILEPATH}" )
 #message( "MSOUTL_CPP=${MSOUTL_CPP}" )
 #message( "MSOUTL_H=${MSOUTL_H}" )
+#GenerateCPPFromFileID( "{2735412F-7F64-5B0F-8F00-5D77AFBE261E}" IMAPI2 im )
+#message( "IMAPI2_TYPEID_FILEPATH=${IMAPI2_TYPEID_FILEPATH}" )
+#message( "IMAPI2_CPP=${IMAPI2_CPP}" )
+#message( "IMAPI2_H=${IMAPI2_H}" )
+
 
 set(qtproject_UIS
 )
 
 set(project_SRCS
-    ${CMAKE_CURRENT_BINARY_DIR}/MSOUTL.cpp
-    ${MSOUTL_OLB}
+    ${MSOUTL_CPP}
+    ${IMAPI2_CPP}
 )
  
 set(qtproject_H
 )
 
 set(project_H
-    ${CMAKE_CURRENT_BINARY_DIR}/MSOUTL.h
+    ${MSOUTL_H}
+    ${IMAPI2_H}
 )
 
 set( project_pub_LIB_DIRS 
